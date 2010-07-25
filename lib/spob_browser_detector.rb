4b39a9d92bed97b299750e6a4cbe9057e4ab0a5c
Bob Sturim <bob@sturim.org>
2010-07-24 22:04:33 -0400

    Revert version #

--- lib/spob_browser_detector.rb
+++ lib/spob_browser_detector.rb
@@ -5,7 +5,7 @@ require 'spob_browser_detector/detector'
 require 'spob_browser_detector/rails/view_helpers'
 
 module SpobBrowserDetector
-  VERSION = '1.0.3'
+  VERSION = '1.0.2'
 end
 
 ActionView::Base.send( :include, SpobBrowserDetector::Rails::ViewHelpers ) if defined?( ActionView )
